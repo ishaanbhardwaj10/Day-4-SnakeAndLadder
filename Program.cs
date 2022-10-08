393a2e1cbf2d5fce76d2285b863c59d2258ca77c
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-08 18:54:22 +0530

    added UC-2 option to switch case in  main method

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Welcome to Snake and Ladder Simulator");
             Console.WriteLine("Use case #1: Single Player at Position Zero");
+            Console.WriteLine("Use case #2: Single Player Rolls the Die");
 
             Console.Write("Choose a use case number to run the program: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -15,6 +16,9 @@
                 case 1:
                     SinglePlayerPositionZero.Play();
                     Console.WriteLine("Player One ready to play, starting at position 0");
+                    break;
+                case 2:
+                    SinglePlayerRollsDie.RollDie();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid use case number!");
